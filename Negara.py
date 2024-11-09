b80693eda0eeaadc09a1296b92e43b7d4ad737d7
Daf_41h <110549338+daf-41h@users.noreply.github.com>
2024-11-09 11:36:13 +0700

    Add files via upload

---
+++ Negara.py
@@ -0,0 +1,15 @@
+class Negara:
+
+  def __init__(self, Nama_Negara, Benua, Jenis_Pemerintahan, Luas_Negara, Jumlah_Penduduk):
+    self.Nama_Negara = Nama_Negara
+    self.Benua = Benua
+    self.Jenis_Pemerintahan = Jenis_Pemerintahan
+    self.Luas_Negara = Luas_Negara
+    self.Jumlah_Penduduk = Jumlah_Penduduk
+
+class Pemimpin:
+  def __init__(self, Jenis_pemimpin, Nama_pemimpin, Tanggal_Lahir_Pemimpin, Ciri_Khas_Pemimpin):
+    self.Jenis_pemimpin = Jenis_pemimpin
+    self.Nama_pemimpin = Nama_pemimpin
+    self.Tanggal_Lahir_Pemimpin = Tanggal_Lahir_Pemimpin
+    self.Ciri_Khas_Pemimpin = Ciri_Khas_Pemimpin
---
+++ Tatasurya.py
@@ -0,0 +1,16 @@
+class Planet:
+  def __init__(self, Nama_Planet, Galaksi, Posisi_Planet, Satelit_Planet, Rotasi_Planet, Revolusi_Planet):
+    self.Nama_Planet = Nama_Planet
+    self.Galaksi = Galaksi
+    self.Posisi_Planet = Posisi_Planet
+    self.Satelit_Planet = Satelit_Planet
+    self.Rotasi_Planet = Rotasi_Planet
+    self.Revolusi_Planet = Revolusi_Planet
+
+class Bintang:
+  def __init__(self, Nama_Bintang, Rasi_Bintang, Jarak_Bintang, Karakteristik_Bintang, Ukuran_Bintang):
+    self.Nama_Bintang = Nama_Bintang
+    self.Rasi_Bintang = Rasi_Bintang
+    self.Jarak_Bintang = Jarak_Bintang
+    self.Karakteristik_Bintang = Karakteristik_Bintang
+    self.Ukuran_Bintang = Ukuran_Bintang
